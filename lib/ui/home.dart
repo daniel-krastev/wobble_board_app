be951069e6c72bdb24b23c0205622475422cfa46
Daniel Krastev <danielkrastev@yahoo.com>
2019-02-10 01:29:19 +0000

    Connecting to device, reading data and decoding the buffer.

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,10 +1,15 @@
 import 'dart:async';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 
+
+const String DEBUG_KEY = "DEBUGDEBUGDEBUGDEBUGDEBUG_";
+
+const int SCAN_CONNECT_TIMEOUT = 6;
+const String CHAR_UUID = "19b10001-e8f2-537e-4f6c-d104768a1214";
 const String SERVICE_UUID = "19b10000-e8f2-537e-4f6c-d104768a1214";
-const String CHARACTERISTIC_UUID = "19b10001-e8f2-537e-4f6c-d104768a1214";
 const String DEVICE_NAME = "Wobbly";
 
 class Home extends StatefulWidget {
@@ -15,18 +20,24 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   FlutterBlue _flutterBlue = FlutterBlue.instance;
 
+  ByteData _buffer;
+  Uint8List _midBuff;
+
   // State
   BluetoothState _bluetoothState = BluetoothState.unknown;
   StreamSubscription _stateSubscription;
 
   // Scanning
   StreamSubscription _scanSubscription;
-  bool _isScanning = false;
+  bool _tryingToConnect = false;
 
   // Device
-  BluetoothDevice wobbly;
-  bool get isConnected => (wobbly != null);
-
+  BluetoothDevice _wobbly;
+  StreamSubscription _deviceConnection;
+  StreamSubscription _deviceStateSubscription;
+  StreamSubscription _charValueSubscription;
+  BluetoothCharacteristic _wobblyChar;
+  BluetoothDeviceState _deviceState = BluetoothDeviceState.disconnected;
 
   @override
   Widget build(BuildContext context) {
@@ -49,7 +60,9 @@ class _HomeState extends State<Home> {
               ),
               RaisedButton(
                 child: Text("Connect"),
-                onPressed: _bluetoothState != BluetoothState.on ? null : _scan,
+                onPressed: _bluetoothState != BluetoothState.on
+                    ? null
+                    : _startConnectionSequence,
               )
             ],
           );
@@ -65,34 +78,16 @@ class _HomeState extends State<Home> {
     super.dispose();
   }
 
-  @override
-  void initState() {
-    super.initState();
-    _flutterBlue.state.then((s) {
-      setState(() => _bluetoothState = s);
-    });
-    _stateSubscription = _flutterBlue.onStateChanged().listen((s) {
-      setState(() => _bluetoothState = s);
-    });
-  }
-
   Widget getAlertTile() {
     return Container(
       color: getInfoTileColor(),
       child: ListTile(
           title: Text(
             getInfoTileString(),
-            style: Theme
-                .of(context)
-                .primaryTextTheme
-                .subhead,
+            style: Theme.of(context).primaryTextTheme.subhead,
           ),
           trailing: Icon(Icons.info,
-              color: Theme
-                  .of(context)
-                  .primaryTextTheme
-                  .subhead
-                  .color)),
+              color: Theme.of(context).primaryTextTheme.subhead.color)),
     );
   }
 
@@ -105,36 +100,86 @@ class _HomeState extends State<Home> {
   }
 
   String getInfoTileString() {
-    return "Bluetooth is ${_bluetoothState.toString()
-        .substring(15)
-        .toUpperCase()}";
+    return "Bluetooth is ${_bluetoothState.toString().substring(15).toUpperCase()}";
   }
 
-  void _scan() {
-    _scanSubscription = _flutterBlue
-        .scan(
-      timeout: const Duration(seconds: 5),
-      withServices: [
-          Guid(SERVICE_UUID)
-        ]
-    )
-        .listen((scanResult) {
-      if(scanResult.device.name == DEVICE_NAME) {
-        wobbly = scanResult.device;
-      }
-    }, onDone: _stopScan);
-
-    setState(() {
-      _isScanning = true;
+  @override
+  void initState() {
+    super.initState();
+    _flutterBlue.state.then((s) {
+      setState(() => _bluetoothState = s);
+    });
+    _stateSubscription = _flutterBlue.onStateChanged().listen((s) {
+      setState(() => _bluetoothState = s);
     });
   }
 
-  _stopScan() {
-    _scanSubscription?.cancel();
-    _scanSubscription = null;
-    setState(() {
-      _isScanning = false;
-    });
+  void _connect() {
+    print("$DEBUG_KEY _connect");
+    if (_wobbly != null) {
+      _deviceConnection = _flutterBlue
+          .connect(_wobbly, timeout: Duration(seconds: SCAN_CONNECT_TIMEOUT))
+          .listen(null, onDone: _disconnect);
 
+      // Subscribe to connection changes
+      _deviceStateSubscription = _wobbly.onStateChanged().listen((s) {
+        _deviceState = s;
+        if (s == BluetoothDeviceState.connected) {
+          _discoverServices();
+        }
+      });
+    }
+  }
+
+  void _decodeBuffer(List<int> valueList) {
+    print(valueList);
+    _midBuff = Uint8List.fromList(valueList);
+    _buffer = _midBuff.buffer.asByteData();
+    print(
+        "X: ${_buffer.getFloat32(1, Endian.little)}; Y: ${_buffer.getFloat32(5, Endian.little)}");
+  }
+
+  _disconnect() {
+    print("$DEBUG_KEY _disconnect");
+    _charValueSubscription?.cancel();
+    _charValueSubscription = null;
+    _deviceStateSubscription?.cancel();
+    _deviceStateSubscription = null;
+    _deviceConnection = null;
+    _deviceConnection?.cancel();
+  }
+
+  void _discoverServices() async {
+    print("$DEBUG_KEY _discoverServices/1");
+    _wobblyChar = (await _wobbly.discoverServices())
+        .firstWhere((s) {
+          return s.uuid.toString() == SERVICE_UUID;
+        })
+        .characteristics
+        .firstWhere((ch) {
+          return ch.uuid.toString() == CHAR_UUID;
+        });
+
+    print("$DEBUG_KEY _discoverServices/2");
+    await _wobbly.readCharacteristic(_wobblyChar).then((v) {
+      _decodeBuffer(v);
+    });
+    print("$DEBUG_KEY _discoverServices/3");
+    await _wobbly.setNotifyValue(_wobblyChar, true);
+    print("$DEBUG_KEY _discoverServices/4");
+    _charValueSubscription = _wobbly.onValueChanged(_wobblyChar).listen((v) {
+      _decodeBuffer(v);
+    });
+  }
+
+  void _startConnectionSequence() async {
+    print("$DEBUG_KEY _startConnectionSequence");
+    _wobbly = (await _flutterBlue.scan(
+            timeout: const Duration(seconds: SCAN_CONNECT_TIMEOUT),
+            withServices: [Guid(SERVICE_UUID)]).firstWhere((scanR) {
+      return scanR.device.name == DEVICE_NAME;
+    }))
+        .device;
+    _connect();
   }
 }
