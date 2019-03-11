6edf50ff2475453df8a58c0774c910913b9fcbbf
ivan_kasabov <iv.kasabov@gmail.com>
2019-03-11 23:52:10 +0000

    smaller buttons

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -53,7 +53,7 @@ class _HomeState extends State<Home> {
                   child: SizedBox(),
                 ),
                 Padding(
-                  padding: const EdgeInsets.all(18.0),
+                  padding: const EdgeInsets.only(right: 18.0),
                   child: Icon(Icons.more_horiz),
                 )
               ],
