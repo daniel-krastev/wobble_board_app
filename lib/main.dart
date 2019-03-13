bff92574a9563ad55c0e46e1c73e60a5a758edd1
Daniel Krastev <danielkrastev@yahoo.com>
2019-03-13 09:44:16 +0000

    Locked in portrait mode. Images problem fixed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:screen/screen.dart';
 import 'package:wobble_board/bloc/app.dart';
 import 'package:wobble_board/bloc/bloc_provider.dart';
@@ -11,41 +12,45 @@ import 'package:wobble_board/ui/pages/settings_page.dart';
 main() {
   Screen.keepOn(true);
   final appBloc = AppBloc();
-  runApp(BlocProvider(
-      bloc: appBloc,
-      child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-              buttonTheme: ButtonThemeData(),
-              backgroundColor: _backgroundColor,
-              primaryColor: _primaryColor,
-              primaryColorLight: _lightTextColor,
-              primaryColorDark: _darkTextColor,
-              iconTheme: IconThemeData(color: _lightTextColor, size: 35.0),
-              fontFamily: "Roboto",
-              primaryTextTheme: TextTheme(
-                title: TextStyle(
-                  color: _primaryColor,
-                  fontSize: 25.0,
-                ),
-                body1: TextStyle(
-                    color: _lightTextColor,
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.w700),
-                body2: TextStyle(
-                    color: _darkTextColor,
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.w500),
-              )),
-          title: "RehApp",
-          routes: <String, WidgetBuilder>{
-            '/about': (_) => About(),
-            '/settings': (_) => Settings(),
-            '/exercise': (_) => ExercisePage(),
-            '/game': (_) => GamePage(),
-          },
-          home: Home())));
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((_) {
+    runApp(BlocProvider(
+        bloc: appBloc,
+        child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+                buttonTheme: ButtonThemeData(),
+                backgroundColor: _backgroundColor,
+                primaryColor: _primaryColor,
+                primaryColorLight: _lightTextColor,
+                primaryColorDark: _darkTextColor,
+                iconTheme: IconThemeData(color: _lightTextColor, size: 35.0),
+                fontFamily: "Roboto",
+                primaryTextTheme: TextTheme(
+                  title: TextStyle(
+                    color: _primaryColor,
+                    fontSize: 25.0,
+                  ),
+                  body1: TextStyle(
+                      color: _lightTextColor,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w700),
+                  body2: TextStyle(
+                      color: _darkTextColor,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w500),
+                )),
+            title: "RehApp",
+            routes: <String, WidgetBuilder>{
+              '/about': (_) => About(),
+              '/settings': (_) => Settings(),
+              '/exercise': (_) => ExercisePage(),
+              '/game': (_) => GamePage(),
+            },
+            home: Home())));
+  });
 }
+
 const Color _backgroundColor = Color.fromARGB(255, 245, 245, 245);
 const Color _darkTextColor = Color.fromARGB(255, 99, 101, 105);
 const Color _lightTextColor = Color.fromARGB(255, 153, 157, 160);
