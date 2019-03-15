88bf6d675520f653ff42636e19ccfa4e3fead1fb
Daniel Krastev <danielkrastev@yahoo.com>
2019-03-15 10:20:02 +0000

    Sounds and dialog for the Exercise page added.

--- lib/bloc/go_page_bloc.dart
+++ lib/bloc/go_page_bloc.dart
@@ -1,19 +1,114 @@
 import 'dart:async';
 
+import 'package:audioplayers/audio_cache.dart';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
+class Exercise {
+  final String title;
+
+  final String hint;
+  final String img;
+  final int reps;
+  final int dur;
+  Exercise(
+      {@required this.title,
+      @required this.hint,
+      @required this.img,
+      this.reps,
+      this.dur});
+}
+
+enum ExerciseEvent { finish, next, update, init, pause, cont }
+
+class ExerciseModel {
+  String image;
+  int currentStep;
+  String hint;
+
+  ExerciseModel({@required this.image, @required this.currentStep, this.hint});
+
+  @override
+  int get hashCode => 1;
+
+  double get progress => ((currentStep + 1) / ExerciseRepo.list.length);
+
+  @override
+  bool operator ==(other) => false;
+}
+
+class ExerciseRepo {
+  static final List<Exercise> list = [
+    Exercise(
+        title: "Standing Balance",
+        hint: "Balance in upright position.",
+        img: "assets/images/exercises/standing_balance.png",
+        dur: 20),
+    Exercise(
+        title: "High Plank",
+        hint: "Keep your hands straight.",
+        img: "assets/images/exercises/high_plank.png",
+        dur: 18),
+    Exercise(
+        title: "Low Plank",
+        hint: "Put your elbows on the board.",
+        img: "assets/images/exercises/low_plank.png",
+        dur: 18),
+    Exercise(
+        title: "One Leg Squat Hold (left)",
+        hint: "Stay on your left leg.",
+        img: "assets/images/exercises/one_leg_squat_hold.png",
+        dur: 8),
+    Exercise(
+        title: "One Leg Squat Hold (right)",
+        hint: "Stay on your right leg.",
+        img: "assets/images/exercises/one_leg_squat_hold.png",
+        dur: 8),
+    Exercise(
+        title: "Push Up (Hands On)",
+        hint: "Keep your elbows close to your chest.",
+        img: "assets/images/exercises/push_ups_hands.png",
+        reps: 10),
+    Exercise(
+        title: "Push Up (Feet On)",
+        hint: "Put your legs on the board and do push ups.",
+        img: "assets/images/trophy.png",
+        reps: 10),
+    Exercise(
+        title: "Single Leg Balance",
+        hint: "Stay on your right leg.",
+        img: "assets/images/exercises/single_leg_balance.png",
+        dur: 10),
+    Exercise(
+        title: "Single Leg Balance",
+        hint: "Stay on your left leg.",
+        img: "assets/images/exercises/single_leg_balance.png",
+        dur: 10),
+    Exercise(
+        title: "Wall Sit",
+        hint: "Put your back against the wall.",
+        img: "assets/images/exercises/wall_sit.png",
+        dur: 10),
+  ];
+}
 
 class GoPageBloc extends Bloc<ExerciseEvent, ExerciseModel> {
   Timer _timer;
   int _currentStep;
+  AudioCache player = AudioCache(prefix: 'sounds/');
+
+  GoPageBloc() {
+    player.loadAll(['cheer.mp3', 'horn.mp3', 'button.mp3', 'tick.mp3']);
+  }
 
   @override
   ExerciseModel get initialState {
-    return InitialExerciseModel(
-      isPaused: false,
-      currentSeconds: 0
-    );
+    return InitialExerciseModel(isPaused: false, currentSeconds: 0);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 
   @override
@@ -24,7 +119,7 @@ class GoPageBloc extends Bloc<ExerciseEvent, ExerciseModel> {
         _timer?.cancel();
         currentState = InitialExerciseModel(
           isPaused: false,
-            currentSeconds: 0,
+          currentSeconds: 0,
         );
         yield (currentState as TimerExerciseModel);
         _currentStep = 0;
@@ -33,8 +128,9 @@ class GoPageBloc extends Bloc<ExerciseEvent, ExerciseModel> {
         });
         break;
       case ExerciseEvent.update:
-        if(currentState is TimerExerciseModel) {
-          if(currentState.peek()) {
+        if (currentState is TimerExerciseModel) {
+          if (currentState.peek()) {
+            player.play("tick.mp3");
             yield currentState..onMoreSecond();
           } else {
             _timer?.cancel();
@@ -43,25 +139,34 @@ class GoPageBloc extends Bloc<ExerciseEvent, ExerciseModel> {
         }
         break;
       case ExerciseEvent.next:
-        if(_currentStep >= ExerciseRepo.list.length) {
+        if (_currentStep >= ExerciseRepo.list.length) {
           dispatch(ExerciseEvent.finish);
         } else {
+          player.play("horn.mp3");
           yield _getModel(_currentStep);
           _currentStep++;
         }
         break;
       case ExerciseEvent.finish:
+        player.play("cheer.mp3");
         yield _getFinishModel();
         break;
       case ExerciseEvent.pause:
-        _timer?.cancel();
-        yield (currentState as TimerExerciseModel)..isPaused = true;
+        if (currentState is TimerExerciseModel) {
+          player.play("button.mp3");
+          _timer?.cancel();
+          yield currentState..isPaused = true;
+        }
         break;
       case ExerciseEvent.cont:
-        _timer = Timer.periodic(Duration(seconds: 1), (t) {
-          dispatch(ExerciseEvent.update);
-        });
-        yield (currentState as TimerExerciseModel)..isPaused = false;
+        if (currentState is TimerExerciseModel) {
+          player.play("button.mp3");
+          _timer?.cancel();
+          _timer = Timer.periodic(Duration(seconds: 1), (t) {
+            dispatch(ExerciseEvent.update);
+          });
+          yield currentState..isPaused = false;
+        }
         break;
     }
   }
@@ -71,88 +176,42 @@ class GoPageBloc extends Bloc<ExerciseEvent, ExerciseModel> {
         currentStep: ExerciseRepo.list.length - 1,
         image: "assets/images/trophy.png",
         reps: null,
-        hint: "Congratulations! Exercises completed!"
-    );
+        hint: "Congratulations! Exercises completed!");
   }
 
   ExerciseModel _getModel(final int count) {
     Exercise e = ExerciseRepo.list[count];
-    if(e.reps != null) {
+    if (e.reps != null) {
       return RepsExerciseModel(
-        currentStep: count,
-        image: e.img,
-        reps: e.reps,
-        hint: e.hint
-      );
+          currentStep: count, image: e.img, reps: e.reps, hint: e.hint);
     } else {
       _timer?.cancel();
       _timer = Timer.periodic(Duration(seconds: 1), (t) {
         dispatch(ExerciseEvent.update);
       });
       return TimerExerciseModel(
-        isPaused: false,
-        currentSeconds: 0,
-        image: e.img,
-        currentStep: count,
-        totalSeconds: e.dur,
-        hint: e.hint
-      );
+          isPaused: false,
+          currentSeconds: 0,
+          image: e.img,
+          currentStep: count,
+          totalSeconds: e.dur,
+          hint: e.hint);
     }
   }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-}
-
-enum ExerciseEvent {finish, next, update, init, pause, cont}
-
-class ExerciseModel {
-  String image;
-  int currentStep;
-  String hint;
-
-  ExerciseModel({
-    @required this.image, @required this.currentStep, this.hint
-  });
-
-  double get progress => ((currentStep + 1) / ExerciseRepo.list.length);
-
-  @override
-  bool operator ==(other) => false;
-
-  @override
-  int get hashCode => 1;
-}
-
-class TimerExerciseModel extends ExerciseModel {
-  int totalSeconds;
-  int currentSeconds;
-  bool isPaused;
-
-  TimerExerciseModel({
-    @required this.totalSeconds,
-    @required this.currentSeconds,
-    @required this.isPaused,
-    @required image,
-    @required currentStep,
-    hint
-  }) : assert(currentSeconds <= totalSeconds),
-        super(image: image, currentStep: currentStep, hint: hint);
-
-  bool peek() => totalSeconds != currentSeconds;
-  onMoreSecond() => currentSeconds++;
-  double get progressTimer => (currentSeconds / totalSeconds);
-  int get remaining => totalSeconds - currentSeconds;
 }
 
 class InitialExerciseModel extends TimerExerciseModel {
   InitialExerciseModel({
     @required currentSeconds,
     @required isPaused,
-  }) : assert(currentSeconds <= 5),
-        super(isPaused: isPaused, totalSeconds: 5, currentSeconds: currentSeconds, image: "assets/images/trophy.png", currentStep: 0, hint: "Get ready to start!");
+  })  : assert(currentSeconds <= 5),
+        super(
+            isPaused: isPaused,
+            totalSeconds: 5,
+            currentSeconds: currentSeconds,
+            image: "assets/images/trophy.png",
+            currentStep: 0,
+            hint: "Get ready to start!");
 
   @override
   double get progress => 0;
@@ -160,41 +219,28 @@ class InitialExerciseModel extends TimerExerciseModel {
 
 class RepsExerciseModel extends ExerciseModel {
   int reps;
-  RepsExerciseModel({
-    @required this.reps,
-    @required image,
-    @required currentStep,
-    hint
-  }) : super(image: image, currentStep: currentStep, hint: hint);
+  RepsExerciseModel(
+      {@required this.reps, @required image, @required currentStep, hint})
+      : super(image: image, currentStep: currentStep, hint: hint);
 }
 
+class TimerExerciseModel extends ExerciseModel {
+  int totalSeconds;
+  int currentSeconds;
+  bool isPaused;
 
-class ExerciseRepo {
-  static final List<Exercise> list = [
-    Exercise(title: "Standing Balance", hint: "Balance in upright position.", img: "assets/images/exercises/standing_balance.png", dur: 20),
-    Exercise(title: "High Plank", hint: "Keep your hands straight.", img: "assets/images/exercises/high_plank.png", dur: 18),
-    Exercise(title: "Low Plank", hint: "Put your elbows on the board.", img: "assets/images/exercises/low_plank.png", dur: 18),
-    Exercise(title: "One Leg Squat Hold (left)", hint: "Stay on your left leg.", img: "assets/images/exercises/one_leg_squat_hold.png", dur: 8),
-    Exercise(title: "One Leg Squat Hold (right)", hint: "Stay on your right leg.", img: "assets/images/exercises/one_leg_squat_hold.png", dur: 8),
-    Exercise(title: "Push Up (Hands On)", hint: "Keep your elbows close to your chest.", img: "assets/images/exercises/push_ups_hands.png", reps: 10),
-    Exercise(title: "Push Up (Feet On)", hint: "Put your legs on the board and do push ups.", img: "assets/images/trophy.png", reps: 10),
-    Exercise(title: "Single Leg Balance", hint: "Stay on your right leg.", img: "assets/images/exercises/single_leg_balance.png", dur: 10),
-    Exercise(title: "Single Leg Balance", hint: "Stay on your left leg.", img: "assets/images/exercises/single_leg_balance.png", dur: 10),
-    Exercise(title: "Wall Sit", hint: "Put your back against the wall.", img: "assets/images/exercises/wall_sit.png", dur: 10),
-  ];
-}
+  TimerExerciseModel(
+      {@required this.totalSeconds,
+      @required this.currentSeconds,
+      @required this.isPaused,
+      @required image,
+      @required currentStep,
+      hint})
+      : assert(currentSeconds <= totalSeconds),
+        super(image: image, currentStep: currentStep, hint: hint);
 
-class Exercise {
-  Exercise(
-      {@required this.title,
-        @required this.hint,
-        @required this.img,
-        this.reps,
-        this.dur});
-
-  final String title;
-  final String hint;
-  final String img;
-  final int reps;
-  final int dur;
+  double get progressTimer => (currentSeconds / totalSeconds);
+  int get remaining => totalSeconds - currentSeconds;
+  onMoreSecond() => currentSeconds++;
+  bool peek() => totalSeconds != currentSeconds;
 }
--- lib/ui/pages/go_exercise_page.dart
+++ lib/ui/pages/go_exercise_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:percent_indicator/linear_percent_indicator.dart';
-import 'package:wobble_board/bloc/go_page_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
 import 'package:wobble_board/bloc/bloc_provider.dart' as native;
+import 'package:wobble_board/bloc/go_page_bloc.dart';
 
 class ExerciseGO extends StatefulWidget {
   @override
@@ -27,7 +27,8 @@ class _ExerciseGOState extends State<ExerciseGO> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     LinearPercentIndicator(
-                      backgroundColor: Theme.of(context).primaryTextTheme.body1.color,
+                      backgroundColor:
+                          Theme.of(context).primaryTextTheme.body1.color,
                       progressColor: Theme.of(context).primaryColor,
                       width: MediaQuery.of(context).size.width,
                       lineHeight: MediaQuery.of(context).size.height * 0.015,
@@ -37,9 +38,15 @@ class _ExerciseGOState extends State<ExerciseGO> {
                 ),
                 Padding(
                   padding: const EdgeInsets.symmetric(vertical: 10.0),
-                  child: Image.asset(mod.image,
-                  width: double.infinity,
-                  height: MediaQuery.of(context).size.height * 0.55),
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: <Widget>[
+                      Image.asset(mod.image,
+                          width: double.infinity,
+                          height: MediaQuery.of(context).size.height * 0.55),
+                      _getIconButton(mod, context)
+                    ],
+                  ),
                 ),
                 Expanded(
                   child: Column(
@@ -47,13 +54,11 @@ class _ExerciseGOState extends State<ExerciseGO> {
                     children: <Widget>[
                       Row(
                           mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            _getStatusWidget(mod)
-                          ]
-                      ),
+                          children: <Widget>[_getStatusWidget(mod)]),
                       Padding(
                         padding: const EdgeInsets.symmetric(vertical: 10.0),
-                        child: Text(mod.hint, style: Theme.of(context).primaryTextTheme.body1),
+                        child: Text(mod.hint,
+                            style: Theme.of(context).primaryTextTheme.body1),
                       ),
                     ],
                   ),
@@ -67,40 +72,31 @@ class _ExerciseGOState extends State<ExerciseGO> {
     );
   }
 
-  Widget _getStatusWidget(ExerciseModel model) {
-    if(model is TimerExerciseModel) {
-      return LinearPercentIndicator(
-        backgroundColor: Theme.of(context).primaryTextTheme.body1.color,
-        progressColor: Theme.of(context).primaryColor,
-        center: Text("${model.remaining}/${model.totalSeconds}", style: Theme.of(context).primaryTextTheme.body2),
-        width: MediaQuery.of(context).size.width * 0.8,
-        lineHeight: 50.0,
-        percent: model.progressTimer,
-      );
-    } else if(model is RepsExerciseModel && model.reps != null){
-      return Text("x${model.reps}", style: Theme.of(context).primaryTextTheme.body1);
-    } else {
-      return Container();
-    }
+  @override
+  void didChangeDependencies() {
+    _bloc = native.BlocProvider.of(context).goPageBloc;
+    _bloc.dispatch(ExerciseEvent.init);
+    super.didChangeDependencies();
   }
 
   Container _getButtons(ExerciseModel model) {
     List<Widget> buttons = [];
-    if(model is TimerExerciseModel) {
+    if (model is TimerExerciseModel) {
       buttons.add(_getPauseCont(model));
       buttons.add(RaisedButton(
         onPressed: () => _bloc.dispatch(ExerciseEvent.next),
         child: Text("Skip"),
       ));
     } else {
-      if((model as RepsExerciseModel).reps != null) {
+      if ((model as RepsExerciseModel).reps != null) {
         buttons.add(RaisedButton(
           onPressed: () => _bloc.dispatch(ExerciseEvent.next),
           child: Text("Done"),
         ));
       } else {
         buttons.add(RaisedButton(
-          onPressed: () => Navigator.of(context).popUntil((r) => r.settings.name == "/"),
+          onPressed: () =>
+              Navigator.of(context).popUntil((r) => r.settings.name == "/"),
           child: Text("Back"),
         ));
       }
@@ -108,14 +104,30 @@ class _ExerciseGOState extends State<ExerciseGO> {
     return Container(
       height: MediaQuery.of(context).size.height * 0.1,
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: buttons
-      ),
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: buttons),
     );
   }
 
+  Widget _getIconButton(ExerciseModel model, BuildContext context) {
+    if (model is RepsExerciseModel && model.reps == null) {
+      return Container();
+    } else {
+      return Positioned(
+        top: 0.0,
+        left: 0.0,
+        child: IconButton(
+            padding: EdgeInsets.all(0),
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              _bloc.dispatch(ExerciseEvent.pause);
+              _showExitDialog(context);
+            }),
+      );
+    }
+  }
+
   RaisedButton _getPauseCont(TimerExerciseModel mod) {
-    if(!mod.isPaused) {
+    if (!mod.isPaused) {
       return RaisedButton(
         onPressed: () => _bloc.dispatch(ExerciseEvent.pause),
         child: Text("Pause"),
@@ -128,10 +140,70 @@ class _ExerciseGOState extends State<ExerciseGO> {
     }
   }
 
-  @override
-  void didChangeDependencies() {
-    _bloc = native.BlocProvider.of(context).goPageBloc;
-    _bloc.dispatch(ExerciseEvent.init);
-    super.didChangeDependencies();
+  Widget _getStatusWidget(ExerciseModel model) {
+    if (model is TimerExerciseModel) {
+      return LinearPercentIndicator(
+        backgroundColor: Theme.of(context).primaryTextTheme.body1.color,
+        progressColor: Theme.of(context).primaryColor,
+        center: Text("${model.remaining}/${model.totalSeconds}",
+            style: Theme.of(context).primaryTextTheme.body2),
+        width: MediaQuery.of(context).size.width * 0.8,
+        lineHeight: 50.0,
+        percent: model.progressTimer,
+      );
+    } else if (model is RepsExerciseModel && model.reps != null) {
+      return Text("x${model.reps}",
+          style: Theme.of(context).primaryTextTheme.body1);
+    } else {
+      return Container();
+    }
+  }
+
+  _showExitDialog(BuildContext context) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (BuildContext ctx) {
+          return Dialog(
+            child: Container(
+              height: 200.0,
+              width: 250.0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Are you sure you want to cancel this practice?",
+                      style: TextStyle(
+                          color: Theme.of(context).primaryColorLight,
+                          fontSize: 20),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      RaisedButton(
+                        child: Text("Quit",
+                            style: Theme.of(context).primaryTextTheme.button),
+                        onPressed: () => Navigator.of(context)
+                            .popUntil((r) => r.settings.name == "/"),
+                      ),
+                      RaisedButton(
+                        child: Text("Continue",
+                            style: Theme.of(context).primaryTextTheme.button),
+                        onPressed: () {
+                          _bloc.dispatch(ExerciseEvent.cont);
+                          Navigator.of(context).pop('dialog');
+                        },
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
